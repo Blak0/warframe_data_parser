e9e211a5e82424eef370ef944d39409ca4e9465b
Blak0 <oblak7@onet.pl>
2020-03-30 20:15:05 +0200

    Tweak Soup and mock test_soup internet connection

--- tests/test_soup.py
+++ tests/test_soup.py
@@ -5,7 +5,13 @@ from warframe_data_parser.soup import Soup
 
 
 class TestSoup(unittest.TestCase):
-    def test_is_borg(self):
+    def setUp(self):
+        # because Soup is a borg we need to set it to predictable state
+        # and then test it
+        self.soup = Soup(lambda: 'any markup')
+
+    @patch('warframe_data_parser.soup.utils.fetch_html_from_repo')
+    def test_is_borg(self, mock_fetch_html):
         s1 = Soup()
         s2 = Soup()
         self.assertIs(s1.soup, s2.soup)
@@ -16,14 +22,14 @@ class TestSoup(unittest.TestCase):
 
     @patch('warframe_data_parser.soup.utils.fetch_html_from_repo')
     def test_get_mission_rows(self, mock_fetch_html):
-        soup = Soup()
-        soup.set_markup(lambda: '<h3 id="missionRewards"></h3><table><tr>Row1</tr><tr>Row2</tr></table>')
-        returns_rows = self.returns_row_lists(soup.get_mission_rows_strings)
+        self.soup.set_markup(
+            lambda: '<h3 id="missionRewards"></h3><table><tr>Row1</tr><tr>Row2</tr></table>')
+        returns_rows = self.returns_row_lists(self.soup.get_mission_rows_strings)
         self.assertTrue(returns_rows)
 
     @patch('warframe_data_parser.soup.utils.fetch_html_from_repo')
     def test_get_relic_rows(self, mock_fetch_html):
-        soup = Soup()
-        soup.set_markup(lambda: '<h3 id="relicRewards"></h3><table><tr>Row1</tr><tr>Row2</tr></table>')
-        returns_rows = self.returns_row_lists(soup.get_relic_rows_strings)
+        self.soup.set_markup(
+            lambda: '<h3 id="relicRewards"></h3><table><tr>Row1</tr><tr>Row2</tr></table>')
+        returns_rows = self.returns_row_lists(self.soup.get_relic_rows_strings)
         self.assertTrue(returns_rows)
--- warframe_data_parser/soup.py
+++ warframe_data_parser/soup.py
@@ -13,17 +13,18 @@ class Borg:
 
 
 class Soup(Borg):
-    def __init__(self):
+    def __init__(self, markup_provider=None):
         super().__init__()
         if 'soup' not in self.__dict__:
-            self.set_markup()
+            self.set_markup(markup_provider)
 
     def get_rows_strings_from_table_id(self, css_id):
         rows = self.soup.select(f'#{css_id} + table > tr')
         return [str(row) for row in rows]
 
-    def set_markup(self, source_callable=utils.fetch_html_from_repo):
-        self.soup = BeautifulSoup(source_callable(), 'lxml')
+    def set_markup(self, provider=None):
+        markup_provider = provider or utils.fetch_html_from_repo
+        self.soup = BeautifulSoup(markup_provider(), 'lxml')
 
     get_mission_rows_strings = partialmethod(
         get_rows_strings_from_table_id,
