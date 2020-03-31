d95867b178dbdfcc1edd8d285b89dccc9fdd4acc
Blak0 <oblak7@onet.pl>
2020-03-31 18:14:12 +0200

    Remove caching on fetch_html_from_repo
    That is because its used in soup borg and caching isn't ever used

--- warframe_data_parser/utils.py
+++ warframe_data_parser/utils.py
@@ -7,13 +7,8 @@ from requests import get
 DROP_REPO = 'https://www.warframe.com/repos/hnfvc0o3jnfvc873njb03enrf56.html'
 
 
-@lru_cache(maxsize=1)
 def fetch_html_from_repo(drop_repo=DROP_REPO):
     """
     Get Warframe's official droprate repository html.
-
-    To not strain the server too much this utility function caches
-    downloaded html data. For this reason if you need to get the most recent
-    version of repo, call the get_drop_html.cache_clear()
     """
     return get(url=drop_repo).text
