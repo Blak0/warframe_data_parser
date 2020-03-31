fee97006947baaf19a8af1f7220e637f38ba9fe0
Blak0 <oblak7@onet.pl>
2020-03-31 16:48:56 +0200

    Simplify soup borg

--- tests/test_soup.py
+++ tests/test_soup.py
@@ -8,12 +8,15 @@ class TestSoup(unittest.TestCase):
     def setUp(self):
         # because Soup is a borg we need to set it to predictable state
         # and then test it
-        self.soup = Soup(lambda: 'any markup')
+        self.soup = Soup()
+        self.soup.set_markup('any markup')
 
     @patch('warframe_data_parser.soup.utils.fetch_html_from_repo')
     def test_is_borg(self, mock_fetch_html):
         s1 = Soup()
+        s1.set_markup('markup')
         s2 = Soup()
+        s2.set_markup('new markup')
         self.assertIs(s1.soup, s2.soup)
 
     def returns_row_lists(self, func):
@@ -22,14 +25,12 @@ class TestSoup(unittest.TestCase):
 
     @patch('warframe_data_parser.soup.utils.fetch_html_from_repo')
     def test_get_mission_rows(self, mock_fetch_html):
-        self.soup.set_markup(
-            lambda: '<h3 id="missionRewards"></h3><table><tr>Row1</tr><tr>Row2</tr></table>')
+        self.soup.set_markup('<h3 id="missionRewards"></h3><table><tr>Row1</tr><tr>Row2</tr></table>')
         returns_rows = self.returns_row_lists(self.soup.get_mission_rows_strings)
         self.assertTrue(returns_rows)
 
     @patch('warframe_data_parser.soup.utils.fetch_html_from_repo')
     def test_get_relic_rows(self, mock_fetch_html):
-        self.soup.set_markup(
-            lambda: '<h3 id="relicRewards"></h3><table><tr>Row1</tr><tr>Row2</tr></table>')
+        self.soup.set_markup('<h3 id="relicRewards"></h3><table><tr>Row1</tr><tr>Row2</tr></table>')
         returns_rows = self.returns_row_lists(self.soup.get_relic_rows_strings)
         self.assertTrue(returns_rows)
